35367bfe9ff0cc7ecd458ede05c8bce1b8e6caa4
leosama <chigozieuduh.cu@gmail.com>
2024-06-12 21:20:26 +0100

    Added stuff in int main to test appendToDll function, taking shape

--- leodatabase/doublyLinkedList.c
+++ leodatabase/doublyLinkedList.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 
 typedef union Value{
@@ -16,10 +16,10 @@ typedef enum datatype{
 
 
 typedef struct node{
-    Value val;
+    Value value;
     datatype type;
-    node* next;
-    node* prev;
+    struct node* next;
+    struct node* prev;
 }node;
 
 typedef struct doublyLL{
@@ -29,7 +29,7 @@ typedef struct doublyLL{
 
 node* createNode(Value val, datatype type){
     node* n = malloc(sizeof(node)*1);
-    n->val = val;
+    n->value = val;
     n->type = type;
     n->next = NULL;
     n->prev = NULL;
@@ -39,5 +39,54 @@ node* createNode(Value val, datatype type){
 doublyLL* createDoublyLL(Value val, datatype type){
     doublyLL* dll = malloc(sizeof(doublyLL) * 1);
     node* n = createNode(val, type);
+    dll->head = n;
+    dll->tail = n;
+    return dll;
+}
+
+void appendToDll(doublyLL* d, Value val, datatype type){
+    node* n = createNode(val, type);
+    node* temp = d->head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = n;
+    n->prev = temp;
+    d->tail = n;
+}
+
+void prependToDll(doublyLL* d, Value val, datatype type){
+    node* n = createNode(val, type);
+    n->next = d->head;
+    d->head = n;
+}
+
+
+
+int main(){
+    Value a, b, c;
+    a.i = 10;
+    b.i = 20;
+    c.i = 30;
+    doublyLL* d = createDoublyLL(a, INT);
+    printf("Doubly linked list has been created\n"
+    "The head of the doubly linked list is at %p wih value %d\n"
+    "The tail of the doubly linked list is at %p wih value %d\n"
+    , d->head, d->head->value.i, d->tail, d->tail->value.i);
+
+    appendToDll(d, b, INT);
+    printf("Doubly linked list has already been created\n"
+    "The head of the doubly linked list is at %p wih value %d\n"
+    "The tail of the doubly linked list is at %p wih value %d\n"
+    , d->head, d->head->value.i, d->tail, d->tail->value.i);
+
+
+    appendToDll(d, c, INT);
+    printf("Doubly linked list has already been created\n"
+    "The head of the doubly linked list is at %p wih value %d\n"
+    "The tail of the doubly linked list is at %p wih value %d\n"
+    , d->head, d->head->value.i, d->tail, d->tail->value.i);
+
+    return 0;
 }
 
