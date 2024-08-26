ddae2352d3d1bbb02e8c13d6127d3ae54efe1204
leosama <chigozieuduh.cu@gmail.com>
2024-08-26 13:55:08 +0100

    header_files folder added for leodatabase

---
+++ leodatabase/header_files/database.h
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+#ifndef DATABASE_H
+#define DATABASE_H
+#include "../src/node.c"
+#include "../src/linkedlist.c"
+
+
+
+node* createNode(value val, datatype type);
+void printNodeRepresentation(node* n);
+
+/*from linkedlist.c*/
+
+unsigned int lengthofLinkedList(node *phead);
+linkedList* createLinkedList(value value, datatype type);
+void appendToLinkedList(
+    linkedList* ll, value val, datatype type
+    );
+void printLinkedList(linkedList* ll);
+node* popNode(node *phead);
+void modifyNode(node *phead, node *currNode, int position);
+void insertNode(node **phead, node *currNode, int position);
+void prepend(node **phead, node * currNode);
+
+
+
+
+#endif
